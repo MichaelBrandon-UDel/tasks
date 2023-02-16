e4f8724ebd2a07e658b494684625e4862104ee04
MichaelBrandon-UDel <70721605+MichaelBrandon-UDel@users.noreply.github.com>
2023-02-15 22:58:58 -0500

    Edited HTML site

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
 import React from "react";
 import "./App.css";
+import witchqueen from "./img/savathun.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript, Michael Arocho
+                <h1>
+                    UD CISC275 with React Hooks and TypeScript, Michael Arocho
+                </h1>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World!
             </p>
+            <img src={witchqueen} alt="The Witch Queen, Savathun" />
+            <ul>
+                <li>Number One</li>
+                <li>Number Two</li>
+                <li>Number Three</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: "100%",
+                                height: "500px"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: "100%",
+                                height: "500px"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
