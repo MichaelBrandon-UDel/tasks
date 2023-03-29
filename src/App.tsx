c8e6b020472fd01ca5b88469e848441efd69d194
MichaelBrandon-UDel <70721605+MichaelBrandon-UDel@users.noreply.github.com>
2023-03-29 01:42:06 -0400

    Adjusted App.tsx more to be 10 compliant

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,9 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): JSX.Element {
     return (
@@ -24,8 +18,7 @@ function App(): JSX.Element {
                     UD CISC275 with React Hooks and TypeScript, Michael Arocho
                 </h1>
             </header>
-            <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
